6e2b5af09630c5c700dfa4284accb31c93044bd9
SebaGutierrezF <sebax00o_123@hotmail.com>
2024-11-25 03:42:04 -0300

    Add missing configuration and translation files

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, addDoc, query, where, getDocs } from 'firebase/firestore';
+import { initializeApp } from '/firebase/app';
+import { getFirestore, collection, addDoc, query, where, getDocs } from '/firebase/firestore';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
