caf336b5846297a17ad73180a7f017c1c7e51447
SebaGutierrezF <sebax00o_123@hotmail.com>
2024-11-25 23:35:32 -0300

    Problema en el firebase-config.js, try 7

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/NumbersHome/',
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://numbers-home-api.onrender.com',
+        changeOrigin: true,
+        secure: true
+      }
+    }
   },
   define: {
     'import.meta.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
